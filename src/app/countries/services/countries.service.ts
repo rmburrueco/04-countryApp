02ae3060d6c6eab0ff7307ff6e0fede9cf4bd307
rmburrueco <rmburrueco@gmail.com>
2023-07-18 17:02:11 +0200

    Capturar errores. Completar por país y por región

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,7 +11,21 @@ export class CountriesService {
     constructor(private http: HttpClient) { }
 
     searchCapital( term: string ) : Observable<Country[]>{
-        return this.http.get<Country[]>( `${this.apiUrl}/capital/${term}`);
+        return this.http.get<Country[]>( `${this.apiUrl}/capital/${term}`)
+        .pipe( catchError( () => of([]) ) )
+        ;
     }
-    
+
+    searchCountry( term: string ) : Observable<Country[]>{
+        return this.http.get<Country[]>( `${this.apiUrl}/name/${term}`)
+        .pipe( catchError( () => of([]) ) )
+        ;
+    }
+
+  searchRegion( term: string ) : Observable<Country[]>{
+      return this.http.get<Country[]>( `${this.apiUrl}/region/${term}`)
+      .pipe( catchError( () => of([]) ) )
+      ;
+  }
+
 }
