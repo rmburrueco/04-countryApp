521fafa9ff2dfa0f8adc361b4529310036837646
rmburrueco <rmburrueco@gmail.com>
2023-07-21 09:21:53 +0200

    Página detalle país. Traducciones

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit{
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -24,11 +27,8 @@ export class CountryPageComponent implements OnInit{
     )
     .subscribe( (country) => { // desesectructuramos -> (params) => { console.log({params: params['id']})}
       //console.log({params: id})
-      if(!country){
-        return this.router.navigateByUrl('');
-      }
-      console.log('TENEMOS UN PAÍS');
-      return;
+      if(!country) return this.router.navigateByUrl('');
+      return this.country = country;
     });
   }
 
