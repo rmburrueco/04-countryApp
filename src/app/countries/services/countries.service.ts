008f2fa994de273f2f57ced0741fbc7dfb69f7b0
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-08-22 20:00:57 +0200

    LocalStorage byCapital y byCountry

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, map, of } from 'rxjs';
-import { Country } from '../interfaces/country';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
     private apiUrl: string = 'https://restcountries.com/v3.1';
+
+    public cacheStore: CacheStore = {
+      byCapital:   { term: '', countries: [] },
+      byCountries: { term: '', countries: [] },
+      byRegion:    { region: '', countries: [] }
+    }
 
     constructor(private http: HttpClient) { }
 
@@ -26,11 +33,17 @@ export class CountriesService {
     }
 
     searchCapital( term: string ) : Observable<Country[]>{
-        return this.getCountriesRequest(`${this.apiUrl}/capital/${term}`);
+        return this.getCountriesRequest(`${this.apiUrl}/capital/${term}`)
+        .pipe(
+          tap( countries => this.cacheStore.byCapital = { term, countries })
+        );
     }
 
     searchCountry( term: string ) : Observable<Country[]>{
-        return this.getCountriesRequest(`${this.apiUrl}/name/${term}`);
+        return this.getCountriesRequest(`${this.apiUrl}/name/${term}`)
+        .pipe(
+          tap( countries => this.cacheStore.byCountries = { term, countries } )
+        );
     }
 
     searchRegion( term: string ) : Observable<Country[]>{
